1fa33eb095419a1645fb948cf99c4963ef329748
fujimaki3968 <s18160@tokyo.kosen-ac.jp>
2021-05-16 11:40:28 +0900

    add update method

---
+++ ch03/contents/update.py
@@ -0,0 +1,28 @@
+import boto3
+
+dynamoDB = boto3.resource('dynamodb')
+sample_table = dynamoDB.Table("meister_chapter03_tutorial")
+
+def lambda_handler(event, context):
+    student_number = event['student_number']
+    food = event['food']
+
+    dynamodb_put_item(student_number, food)
+
+    # TODO implement
+    return {
+        'statusCode': 200,
+        'body': 'success'
+    }
+
+
+def dynamodb_update_item(id: str, food: str):
+    sample_table.put_item(
+        Key={
+            'id': id,
+        },
+        UpdateExpression="set food=:f",
+        ExpressionAttributeValues={
+            ':f': food,
+        },
+    )
